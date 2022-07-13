ceb9fb4e1450be1192f93d4c3c6ba632f07f5b85
Artem <artinnok@mail.ru>
2022-07-13 20:22:27 +0600

    рефакторинг

--- models.py
+++ models.py
@@ -1,18 +1,9 @@
 from peewee import *
-from environs import Env
 
-environment = Env()
-environment.read_env()
-
-DB_NAME = environment("DB_NAME")
-DB_URI = environment("DB_URI")
+from config import get_db
 
 
-db = PostgresqlDatabase(
-    database=DB_NAME,
-    dsn=DB_URI,
-)
-db.connect()
+db = get_db()
 
 
 class Person(Model):
