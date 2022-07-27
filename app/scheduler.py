3401fb1afca33fbf8ba69e67a7b9a33d9c4a84a0
Artem <artinnok@mail.ru>
2022-07-27 10:42:05 +0600

    переделал скедулер

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,7 +1,3 @@
-from time import sleep
-
-import schedule
-
 from tasks import hello_world
 from dependencies import get_queue
 
@@ -11,9 +7,5 @@ def enqueue_task():
     queue.enqueue(hello_world)
 
 
-schedule.every(10).seconds.do(enqueue_task)
-
 if __name__ == "__main__":
-    while True:
-        schedule.run_pending()
-        sleep(1)
+    enqueue_task()
